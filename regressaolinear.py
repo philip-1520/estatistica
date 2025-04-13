14b6ae23c783335af4884a0f46746c51df343d21
Philip Ribeiro Lima <ribeirolima1520@gmail.com>
2025-04-13 12:10:54 -0300

    Update regressaolinear.py

--- regressaolinear.py
+++ regressaolinear.py
@@ -12,37 +12,28 @@ Retorna:
 """
 
 import pandas as pd
-from decimal import Decimal, getcontext
 
-def regressaolinear(dados_originais: pd.DataFrame, x: str, y: str, iteracoes: int = 1000, precisao: int = 2) -> tuple[float, float]:
-
-    # Define a precisão de 50 casas decimais.
-    getcontext().prec = 50
+def regressaolinear(dados: pd.DataFrame, x: str, y: str, iteracoes: int = 1000, precisao: int = 2):
     
-    # Transforma os dados originais para decimal.
-    dados = pd.DataFrame()
-    dados[x] = dados_originais[x].apply(lambda x: Decimal(str(x)))
-    dados[y] = dados_originais[y].apply(lambda y: Decimal(str(y)))
-
-    # Declara a, b e uma constante dois como decimal.
-    a = Decimal(1.0)
-    b = Decimal(0.0)
-    dois = Decimal(2.0)
+    # Estima os valores iniciais de a e b utilizando os dois primeiros pontos.
+    a = (dados.loc[1, y] - dados.loc[0, y])/(dados.loc[1, x] - dados.loc[0, x])
+    b = dados.loc[0, y] - a*dados.loc[0, x]
 
     # Calcula a segunda derivada parcial da soma dos quadrados dos desvios em relação aos coeficientes a e b.
-    dda = (dois*dados[x]**dois).sum()
-    ddb = dois*len(dados[x])
+    dda = (2*dados[x]**2).sum()
+    ddb = 2*len(dados[x])
 
     # O algoritmo aproxima as raízes da primeira derivada parcial de cada coeficiente utilizando o método de Newton-Raphson.
     for i in range(iteracoes):
     
         # Calcula a primeira derivada parcial da soma dos quadrados dos desvios em relação aos coeficientes a e b.
-        da = ((-dois*dados[x])*(dados[y] - a*dados[x] - b)).sum()
-        db = (-dois*(dados[y] - a*dados[x] - b)).sum()
+        da = ((-2*dados[x])*(dados[y] - a*dados[x] - b)).sum()
+        db = (-2*(dados[y] - a*dados[x] - b)).sum()
 
         # Atualiza os valores de a e b para se aproximarem das raízes das funções.
         a = a - da/dda
         b = b - db/ddb
 
     # Retorna os valores dos coeficientes arredondados.
-    return round(float(a), precisao), round(float(b), precisao)
+    return round(a, precisao), round(b, precisao)
+    
